47435cf3a0de4e1974998eec7b9a0ee47e04c799
David Morales <david@davidmles.com>
2024-02-17 12:13:58 +0000

    Use Turbo Streams to display the new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to posts_path, notice: "Post was successfully created." }
         format.json { render :index, status: :created, location: @post }
+        format.turbo_stream { flash.now[:notice] = "Post was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
